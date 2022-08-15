7519d3fae803dea9ca3e116c840011ef46392765
akiowaki <owakiakihiro@gmail.com>
2022-08-15 11:43:15 +0900

    firebase has been added.

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAg0kY42UuZKbe1Wq6a_htZDbPI9P6MslQ",
+  authDomain: "netflix-next-ts-tailwind.firebaseapp.com",
+  projectId: "netflix-next-ts-tailwind",
+  storageBucket: "netflix-next-ts-tailwind.appspot.com",
+  messagingSenderId: "344933888503",
+  appId: "1:344933888503:web:a7394f1f78dbba251e7ef2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
