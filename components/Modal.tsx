f9cac32d0611fc01b57dc4edeaef3bdc95756003
akiowaki <owakiakihiro@gmail.com>
2022-08-15 18:35:07 +0900

    Fixed a bug regarding scrolling

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -60,8 +60,10 @@ function Modal() {
         className="fixed !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
       >
         <>
-                <button onClick={handleClose} 
-                className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818]">
+        <button
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+          onClick={handleClose}
+        >
                     <XIcon className="h-6 w-6"/>
                 </button>
                 <div className="reactive pt-[56.25%]">
